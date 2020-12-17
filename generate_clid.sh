7a0c423ce2dc15bfd0606dbb5b75889d6d86b5f2
michael <mvachette@nuxeo.com>
2020-12-17 15:27:34 +0900

    Use TMP dir for instance registration

--- generate_clid.sh
+++ generate_clid.sh
@@ -9,6 +9,8 @@ set -euf
 NUXEO_ENV=".env"
 ERR=""
 
+TMP_DIR="/tmp/nuxeo"
+
 NUXEO_CLID=$(grep NUXEO_CLID ${NUXEO_ENV} | cut -d '=' -f2)
 if [ -n "${NUXEO_CLID}" ]; then
   echo "NUXEO_CLID appears to be configure in ${NUXEO_ENV} or your system environment.  Remove and then run this script again."
@@ -41,10 +43,10 @@ if [ -n "${ERR}" ]; then
   exit 1
 fi
 
-docker run -it -v /var/lib/nuxeo/:/var/lib/nuxeo/:rw ${FROM_IMAGE} \
+docker run -it -v ${TMP_DIR}:/var/lib/nuxeo/:rw ${FROM_IMAGE} \
        nuxeoctl register "${STUDIO_USERNAME}" "${APPLICATION_NAME}" "dev" "Docker" "${STUDIO_CREDENTIALS}"
-if [ -e /var/lib/nuxeo/instance.clid ]; then
+if [ -e ${TMP_DIR}/instance.clid ]; then
   echo -n "NUXEO_CLID=" >> ${NUXEO_ENV}
-  awk 1 ORS="--" /var/lib/nuxeo/instance.clid >> ${NUXEO_ENV}
+  awk 1 ORS="--" ${TMP_DIR}/instance.clid >> ${NUXEO_ENV}
   echo "" >> ${NUXEO_ENV}
 fi
