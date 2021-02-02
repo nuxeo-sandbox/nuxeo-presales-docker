832b734b4a5d3af22d3d99570c7b0772ba037400
Damon Brown <dbrown@nuxeo.com>
2021-02-02 10:34:28 -0500

    Add log & debug config
    Add example configuration for debug, mail as well as exposing ports.  Handle log4j2.xml changes by copying the current state to init folder.

--- init/0_register.sh
+++ init/0_register.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+# Alternative way to register instance
 if [ ! -e /var/lib/nuxeo/instance.clid ]; then
   echo "Registering Nuxeo instance with project ${APPLICATION_NAME}"
   nuxeoctl register ${STUDIO_USERNAME} "${APPLICATION_NAME}" "dev" "docker" "${STUDIO_CREDENTIALS}"
 fi
 
+# Install pending hotfixes for image
 if [ ${NUXEO_INSTALL_HOTFIX:='true'} == "true" ]; then
   echo "Installing hotfixes..."
   nuxeoctl mp-hotfix --accept=true
 fi
 
-if [ -n "${ADDITIONAL_PACKAGES}" ]; then
-  echo "Installing additional packages..."
-  nuxeoctl mp-install --accept=true ${ADDITIONAL_PACKAGES}
+# Copy log configuration for debugging, etc.
+if [ -e /docker-entrypoint-initnuxeo.d/log4j2.xml ]; then
+  echo "Copying log configuration from init folder to server"
+  cp -vf /docker-entrypoint-initnuxeo.d/log4j2.xml /opt/nuxeo/server/lib/log4j2.xml
+elif [ -e /opt/nuxeo/server/lib/log4j2.xml ]; then
+  echo "Copying server log configuration to init folder"
+  cp -vf /opt/nuxeo/server/lib/log4j2.xml /docker-entrypoint-initnuxeo.d/log4j2.xml
 fi
