1136738ddc182900a6f96b46851a49fedd3493d8
Josh Fletcher <josh.fletcher@hyland.com>
2023-04-28 12:25:37 -0700

    Fix pulling usage.

--- aliases.sh
+++ aliases.sh
@@ -12,7 +12,7 @@ alias dcl='docker compose logs'
 alias dclf='docker compose logs -f'
 alias dco=docker compose
 alias dcps='docker compose ps'
-alias dcpull='docker compose pull'
+alias dcpull='docker compose build --pull'
 alias dcr='docker compose run'
 alias dcrestart='docker compose restart'
 alias dcrm='docker compose rm'
@@ -31,10 +31,7 @@ alias nxbash='nx exec COMMAND=bash'
 alias es='stack SERVICE=elasticsearch'
 alias mongodb='stack SERVICE=mongo'
 alias mongo='stack exec SERVICE=mongo COMMAND=mongo'
-# This is a hard-coded hack for now; the root problem is `nx pull` does nothing
-# useful because we're using a custom image. What we want to do is pull the
-# latest base nuxeo image, and then build a new local image.
-alias nxpull='docker pull docker-private.packages.nuxeo.com/nuxeo/nuxeo:2021'
+alias nxpull='docker compose build --pull'
 
 # Quick access to nuxeoctl
 alias nxctl-status='nx exec COMMAND="nuxeoctl status"'
