bc9f734a4ad29a784d5d294673641bd13506320b
Josh Fletcher <josh.fletcher@hyland.com>
2026-09-15 10:30:40 -0700

    Not relevant here.

--- aliases.sh
+++ aliases.sh
@@ -24,7 +24,6 @@ alias dcupd='docker compose up -d'
 # QOL aliases to make managing the stack easier
 alias stack='make -e'
 alias nx='stack SERVICE=nuxeo'
-# See https://github.com/nuxeo-sandbox/nuxeo-presales-docker/issues/10
 alias nxl='nx exec COMMAND="tail -Fn+1 /var/log/nuxeo/server.log"'
 alias vilog='nx exec COMMAND="vi /var/log/nuxeo/server.log"'
 alias nxbash='nx exec COMMAND=bash'
